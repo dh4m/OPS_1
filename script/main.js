5dc0b567d37d78e6806fe2d2769268ed55dc49cf
dham <dham@student.42seoul.kr>
2023-11-20 22:12:51 +0900

    [init] main.js file create and Code for displaying song charts on the display

---
+++ script/main.js
@@ -0,0 +1,32 @@
+let chart_list = [];
+
+
+const refresh_list = () => {
+	// 여기 대충 실시간 차트 받아오는 코드 ㅇㅇ 배열 chart_list에 10개 채우면 됨
+	chart_list = [
+		"good music 1", "good music 2", "good music 3", "good music 4", "good music 5", 
+		"good music 6", "good music 7", "good music 8", "good music 9", "good music 10"
+	]; // 임시 코드
+	return ;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	refresh_list();
+	let i = 1;
+	let count = 0;
+	let chart_div = document.getElementById("realtime-chart");
+
+	chart_div.textContent = chart_list[0];
+	setInterval(() => {
+		if (count >= 45) { // 3분마다 실시간 차트 받아옴
+			count = 0;
+			refresh_list();
+		}
+		else count++;
+		chart_div.textContent = chart_list[i];
+		chart_div.style.animation = 'none'; // 애니메이션 리셋
+    	chart_div.offsetHeight; // 리플로우 발생
+    	chart_div.style.animation = 'slideUp 0.5s ease-in-out'; // 애니메이션 재적용
+		i = (i + 1) % chart_list.length;
+	}, 4000);
+});
